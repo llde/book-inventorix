2c76118364a865f160affbcb84849dee4b237981
Lorenzo <lorenzofersteam@live.it>
2015-09-05 12:06:48 +0200

    Still More program structures.
    Continued the callbacks

--- src/GUI/CreateItem/CreateItem.java
+++ src/GUI/CreateItem/CreateItem.java
@@ -1,14 +1,29 @@
 package GUI.CreateItem;
 
+import Back.Property;
 import GUI.Interface.GUI;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 /**
  * Created by Lorenzo on 31/08/2015.
  */
 public class CreateItem implements GUI{
+    @FXML
+    private Button ButtonOk;
+    @FXML
+    private Button ButtonAbort;
+    @FXML
+    private TableView<Property> TableProperty;
+    @FXML
+    private TextField NameText;
+
+
     private Scene sc = null;
     public CreateItem(){
         try {
