f4becb9d4f49dd501fc385b91afd4e670bf956c0
Lorenzo <lorenzofersteam@live.it>
2016-01-19 22:59:10 +0100

    config

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,33 @@
 import Back.Item;
 import Back.ItemImpl;
 import GUI.UIDispatcher;
+import Utils.Configuration;
 import javafx.application.Application;
+import javafx.application.Preloader;
 import javafx.stage.Stage;
 
 /**
  * Created by Lorenzo on 30/08/2015.
  */
 public class Main  extends Application{
+
     public static void main(String[] args){
         //PreFX configurationfile init.
+        //Preloader
+        PreFX();
         launch(args);
         //PostFX  hook crash javaFX thread
+    }
+
+
+    public static void PreFX(){
+        Configuration.getConfigurator();
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         UIDispatcher dis = UIDispatcher.getDispatcher();
+        primaryStage.setTitle( Configuration.getConfigurator().title + " " + Configuration.getConfigurator().version);
         primaryStage.setScene(dis.getMainwin().getScene());
         primaryStage.show();
     }
