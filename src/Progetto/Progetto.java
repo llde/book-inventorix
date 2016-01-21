3cfec587b33104ae2ddd34caaf2035cfb9384f37
Lorenzo Ferrillo <lorenzofersteam@live.it>
2016-01-21 17:18:32 +0100

    progetto inteface

--- src/Progetto/Progetto.java
+++ src/Progetto/Progetto.java
@@ -1,9 +1,29 @@
 package Progetto;
 
+import Back.Item;
+import Back.Property;
+
+import java.util.List;
+import java.util.UUID;
+
 /**
  * Created by Lorenzo on 19/01/2016.
  */
-public class Progetto {
+public interface Progetto {
+
+    List<Item> getItems();
+
+    List<Property<?>> getProperty();
+
+    void addItem(Item it);
+
+    <T> void addProperty(Property<T> property);
+
+    UUID getUUID();
+
+    <T> T save();
+
+    void dispose();
 
 
 
